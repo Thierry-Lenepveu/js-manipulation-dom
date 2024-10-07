16d5463a57f83d806220000d19aea7088637fd8c
Eunomia <eunomia.dice@gmail.com>
2024-10-07 13:58:03 +0200

    Initial project.

---
+++ script.js
@@ -0,0 +1,30 @@
+import "./style.css";
+
+const animalsToAdopt = [
+    {
+      name: "Lucky",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Tiffanie_at_cat_show.jpg/199px-Tiffanie_at_cat_show.jpg"
+    },
+    {
+      name: "Symba",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Tiffany4_%282018%3B_cropped_2023%29.jpg/240px-Tiffany4_%282018%3B_cropped_2023%29.jpg"
+    },
+    {
+      name: "Léo",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Norskskogkatt_Evita_3.JPG/245px-Norskskogkatt_Evita_3.JPG"
+    },
+    {
+      name: "Milo",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/CyprusShorthair.jpg/320px-CyprusShorthair.jpg"
+    },
+    {
+      name: "Charly",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Highlander-7.jpg/293px-Highlander-7.jpg"
+    }
+  ];
+  
